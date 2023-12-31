72216c0106fd78290a2d77ff2bcfd0198e23b393
Simon Hallin <simon.karlerik@outlook.com>
2023-12-31 11:00:32 +0100

    Finished base of binary tree with functionality for Pre Order Traversal

--- tree.js
+++ tree.js
@@ -1,3 +1,5 @@
+"use strict"
+
 class TreeNode {
     constructor(key, value = key, parent = null) {
         this.key = key;
@@ -14,11 +16,47 @@ class TreeNode {
 }
 
 class BinaryTree {
-    constructor(rootNode = null) {
-        this.rootNode = rootNode;
+    constructor(root = null) {
+        this.root = root;
     }
 
-    insert(parent, key, value, rightNode = true) {
-        // Check whether right/left node is already inserted 
+    *PreOrderTraversal(node = this.root) {
+        yield node;
+        if (node.left !== null) yield* this.PreOrderTraversal(node.left);
+        if (node.right !== null) yield* this.PreOrderTraversal(node.right);
+    }
+
+    find(key) {
+        // Might be a bit ineffective, but works for this purpose
+        for (let node in this.PreOrderTraversal()) {
+            if (node.key === key) {
+                return node;
+            }
+        }
+        return undefined;
+    }
+
+    insert(parent, key, value) {
+        parentNode = this.find(parent);
+        if (parentNode.left === null) {
+            parentNode.left = TreeNode(key, value, parentNode)
+            return true;
+        }
+        if (parentNode.right === null) {
+            parentNode.right = TreeNode(key, value, parentNode)
+            return true;
+        }
+        return false;
     }
 }
+
+// ((2 * (3 +5)) / 7 + 9) * 7 - 6 + (3 - 2 * 4)
+// 2 * 3 * 4
+// 2 / 3 + 4 + 2 * 3 + 4
+
+// Go through till you find a + or -, this will be the last value evaluated
+// (2 + 3 * 4) if the entire expression is encapsulated in (), remove them.
+// ( ) can be seen as it's own constant that will be further splitted later
+// if + and - cannot be found, look for multiplication then division
+
+// ((2 * (3 + 5)) / 7 + 9) * 7 - 6 + (3 - 2 * 4)
